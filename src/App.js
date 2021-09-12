84a53f75f7a274d3d12cb517d45111b8e3c52ed7
Kris Renaldi <krenaldi@comcast.net>
2021-09-12 15:19:33 -0400

    added toggle function to cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState } from 'react'
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [openCart, setOpenCart] = useState(false);
+
+  function showCartHandler() {
+    setOpenCart(true)
+  }
+
+  function hideCartHandler() {
+    setOpenCart(false)
+  }
+
   return (
     <CartProvider>
+      {cartOpen && <div>Cart...</div>}
       <Header />
       <main>
         <Meals />
